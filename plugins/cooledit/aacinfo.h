06c13cacf311866c5c59cb6ccfa05bb49e6a15d0
menno <menno>
2001-10-16 13:24:05 +0000

    Bugfixes by 4N

--- plugins/cooledit/aacinfo.h
+++ plugins/cooledit/aacinfo.h
@@ -16,7 +16,7 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * $Id: aacinfo.h,v 1.1 2001/10/11 09:53:44 menno Exp $
+ * $Id: aacinfo.h,v 1.2 2001/10/16 13:24:15 menno Exp $
  */
 
 typedef struct {
