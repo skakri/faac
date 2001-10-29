45551af6ade5a6e30bae297e399d7fa853079d5a
menno <menno>
2001-10-29 10:01:31 +0000

    Updated cooledit plugin, now working correctly with new library interface

--- plugins/cooledit/aacinfo.c
+++ plugins/cooledit/aacinfo.c
@@ -16,7 +16,7 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * $Id: aacinfo.c,v 1.2 2001/10/16 13:24:05 menno Exp $
+ * $Id: aacinfo.c,v 1.3 2001/10/29 10:01:31 menno Exp $
  */
 
 #include <windows.h>
