2b470d1c7e17bb66143cb641aedee20b1457bfe4
menno <menno>
2002-04-20 15:08:27 +0000

    Updated cooledit code plugin now supports MP4 files

--- plugins/cooledit/aacinfo.h
+++ plugins/cooledit/aacinfo.h
@@ -16,7 +16,7 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * $Id: aacinfo.h,v 1.4 2002/04/17 18:43:24 menno Exp $
+ * $Id: aacinfo.h,v 1.5 2002/04/20 15:08:27 menno Exp $
  */
 
 typedef struct {
